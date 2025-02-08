8778eaa2002999c86545515d58c2f5a102d1cbec
Victor Binzar <a12022144@unet.univie.ac.at>
2025-02-08 15:09:18 +0200

    initial

---
+++ src/main/java/ro/vic/bet_exchange/BetExchangeApplication.java
@@ -0,0 +1,13 @@
+package ro.vic.bet_exchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BetExchangeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BetExchangeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ro/vic/bet_exchange/BetExchangeApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.vic.bet_exchange;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BetExchangeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
