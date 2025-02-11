7eb51656fedfff177d39a2b75d5ed3003c93fc6f
Victor Binzar <a12022144@unet.univie.ac.at>
2025-02-11 20:17:11 +0200

    Simple explanation for Martingale formula

--- src/main/java/ro/vic/bet_exchange/util/MartingaleCalculator.java
+++ src/main/java/ro/vic/bet_exchange/util/MartingaleCalculator.java
@@ -2,6 +2,7 @@ package ro.vic.bet_exchange.util;
 
 public class MartingaleCalculator {
     // Returns next stake if last bet failed, given initial stake and number of losses
+    // Geometry progression: nextStake = initialStake * 2^(lossCount)
     public static double calculateNextStake(double initialStake, int lossCount) {
         // With doubling: nextStake = initialStake * 2^(lossCount)
         return initialStake * Math.pow(2, lossCount);
