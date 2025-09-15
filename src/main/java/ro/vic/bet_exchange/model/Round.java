4170252e53d2cf5fab71654071c2201549e8ed5a
Victor Binzar <a12022144@unet.univie.ac.at>
2025-09-15 22:20:44 +0300

    Added Round entity to hold history of matches

--- src/main/java/ro/vic/bet_exchange/model/Round.java
+++ src/main/java/ro/vic/bet_exchange/model/Round.java
@@ -1,29 +1,29 @@
 package ro.vic.bet_exchange.model;
 
 import jakarta.persistence.*;
-import lombok.Data;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import java.time.LocalDateTime;
+import java.util.List;
 
 @Entity
-@Table(name = "bets")
+@Table(name = "rounds")
 @NoArgsConstructor
 @Getter
 @Setter
-public class Bet {
+public class Round {
+    public final int MAX_SEASON_ROUND = 30;
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @ManyToOne
-    private Match match;
 
-    private double stake;
-    private double odds;
-    private String status; // e.g., PENDING, WIN, LOSS
-    private LocalDateTime betTime;
+    // Holds each 8 matches in a round
+    private List<Match> matches;
+
+    // Counts to 30 TODO: split in 2 groups
+    private int SeasonRound;
 
 }
